407a96dec955884bd9e0f5c826d7c37b6a72a5a9
CharleyMa <charleyma@gmail.com>
2015-06-24 15:13:17 -0700

    Added in web hook
    web hooks added as a test…

---
+++ app.js
@@ -0,0 +1,102 @@
+var express       = require('express');
+var bodyParser    = require('body-parser');
+var request       = require('request');
+var dotenv        = require('dotenv');
+var SpotifyWebApi = require('spotify-web-api-node');
+var Slack = require('slack-node');
+
+dotenv.load();
+
+var spotifyApi = new SpotifyWebApi({
+  clientId     : process.env.SPOTIFY_KEY,
+  clientSecret : process.env.SPOTIFY_SECRET,
+  redirectUri  : process.env.SPOTIFY_REDIRECT_URI
+});
+
+var slack = new Slack();
+slack.setWebhook(process.env.SLACK_WEBHOOK_URL);
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', function(req, res) {
+  if (spotifyApi.getAccessToken()) {
+    return res.send('You are logged in.');
+  }
+  return res.send('<a href="/authorise">Authorise</a>');
+});
+
+app.get('/authorise', function(req, res) {
+  var scopes = ['playlist-modify-public', 'playlist-modify-private'];
+  var state  = new Date().getTime();
+  var authoriseURL = spotifyApi.createAuthorizeURL(scopes, state);
+  res.redirect(authoriseURL);
+});
+
+app.get('/callback', function(req, res) {
+  spotifyApi.authorizationCodeGrant(req.query.code)
+    .then(function(data) {
+      spotifyApi.setAccessToken(data.body['access_token']);
+      spotifyApi.setRefreshToken(data.body['refresh_token']);
+      return res.redirect('/');
+    }, function(err) {
+      return res.send(err);
+    });
+});
+
+app.use('/store', function(req, res, next) {
+  if (req.body.token !== process.env.SLACK_TOKEN) {
+    return res.status(500).send('Cross site request forgerizzle!');
+  }
+  next();
+});
+
+app.post('/store', function(req, res) {
+  spotifyApi.refreshAccessToken()
+    .then(function(data) {
+      spotifyApi.searchTracks(req.body.text)
+        .then(function(data) {
+          var results = data.body.tracks.items;
+          if (results.length === 0) {
+            return res.send('Could not find that track.');
+          }
+          var track = results[0];
+          spotifyApi.addTracksToPlaylist(process.env.SPOTIFY_USERNAME, process.env.SPOTIFY_PLAYLIST_ID, ['spotify:track:' + track.id])
+            .then(function(data) {
+              console.log('full track:');
+              console.log(track);
+              console.log('===========');
+              console.log('track artists:')
+              console.log(track.artists);
+              console.log('artist name:');
+              console.log(track.artists[0].name);
+              console.log('track name:');
+              console.log(track.name);
+              var artistName = track.artists[0].name;
+              var trackName = track.name;
+               slack.webhook({
+                channel: "#jukebot",
+                username: "jukebot",
+                text: "Track " + trackName + " by " + artistName + " added to playlist."
+                }, function(err, response) {
+                  console.log(response);
+                });
+
+
+              return res.send('Track added: *' + track.name + '* by *' + track.artists[0].name + '*');
+            }, function(err) {
+              return res.send(err.message);
+            });
+        }, function(err) {
+          return res.send(err.message);
+        });
+    }, function(err) {
+      return res.send('Could not refresh access token. You probably need to re-authorise yourself from your app\'s homepage.');
+    });
+});
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
